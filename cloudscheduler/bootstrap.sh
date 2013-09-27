bf79f3a12afb774e146e1d14842938d8461d9a15
Frank Berghaus <frank.berghaus@gmail.com>
2013-09-27 14:32:55 -0700

    Modified bootstapping sctipt to use fork of atlas-gcemoduels
    Clone spiiph's project to allow modified startup scripts on GCE. Aim to
    reinegrate with spiiph's repository as soon as possible.  Make tow edits
    for bootstapping script.  Fetch modules from MadMaloclm's repository.
    Change node_template to the one used for IAAS cloud scheduler nodes.

--- cloudscheduler/bootstrap.sh
+++ cloudscheduler/bootstrap.sh
@@ -42,36 +42,15 @@ __EOF__
 
 
 cat << "__EOF__" > $node_template
-# Mounts and node template for a gce worker node
-
-# Cache for CVMFS
-mount {'/var/cache/cvmfs2':
-    device => '/dev/vg00/lv_cvmfs',
-    fstype => 'ext4',
-    options => 'defaults',
-    ensure => mounted,
-    dump => 1,
-    pass => 2,
-}
-
-# Cache for scheduler
-mount {'/var/lib/condor':
-    device => '/dev/vg00/lv_condor',
-    fstype => 'ext4',
-    options => 'defaults',
-    ensure => mounted,
-    dump => 1,
-    pass => 2,
-}
-
 class { 'gce_node':
   head => 'to.be.contextualized.by.cloud.scheduler',
   role => 'csnode',
+  use_cvmfs => true,
   condor_pool_password => undef,
   condor_use_gsi => true,
-  condor_slots => 1,
+  condor_slots => 32,
   use_xrootd => false,
-  require => Mount['/var/cache/cvmfs2', '/var/lib/condor'],
+  cvmfs_domain_servers => "http://cvmfs.racf.bnl.gov:8000/opt/@org@;http://cvmfs.fnal.gov:8000/opt/@org@;http://cvmfs-stratum-one.cern.ch:8000/opt/@org@;http://cernvmfs.gridpp.rl.ac.uk:8000/opt/@org@;http://cvmfs02.grid.sinica.edu.tw:8000/opt/@org@"
 }
 __EOF__
 
@@ -108,7 +87,7 @@ message "Formatting and mounting extra ephemeral disks..."
 sh $mount_script
 
 message "Fetching puppet modules for GCE..."
-git clone https://github.com/spiiph/atlasgce-modules.git /etc/puppet/modules
+git clone https://github.com/MadMalcolm/atlasgce-modules.git /etc/puppet/modules
 
 # Install puppet modules after having cloned the atlasgce modules, since git
 # will balk at cloning to a non-empty directory
